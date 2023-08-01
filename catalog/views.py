a12399a6de5bbcbfbcdbbeae6ba06df9e7eb2ce8
Евгений <xarvat@list.ru>
2023-08-01 20:29:50 +0300

    исправление ошибок

--- catalog/views.py
+++ catalog/views.py
@@ -9,9 +9,9 @@ def contacts(request):
     if request.method == 'POST':
         data = {}
         name = request.POST.get('name')
-        email = request.POST.get('email')
+        phone = request.POST.get('phone')
         message = request.POST.get('message')
 
-        data['user_1'] = (name, email, message)
+        data['user_1'] = (name, phone, message)
         print(data)
     return render(request, 'catalog/contacts.html')
