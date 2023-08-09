c96e495f173c8d391861d8d1687a4e95c678cada
Евгений <xarvat@list.ru>
2023-08-09 20:19:45 +0300

    добавление шаблонов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.views import index, contacts, ProductDetailView
 
 urlpatterns = [
     path('', index),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+
+from catalog.models import Product
+from django.shortcuts import get_object_or_404, redirect, render
 
 
 def index(request):
     return render(request, 'catalog\index.html')
+
+
+class ProductDetailView(View):
+    template_name = 'catalog/product_detail.html'
+
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        return render(request, self.template_name, {'product': product})
 
 
 def contacts(request):
